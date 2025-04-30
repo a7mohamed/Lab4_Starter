af44f162138cfcfbcf9ac307ac5bfd9fecd11ae7
Abdirahman Mohamed <114545465+a7mohamed@users.noreply.github.com>
2025-04-30 07:21:21 -0700

    Update lab4.js #2

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,9 +6,12 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || typeof add !== 'boolean') {
+        return false;
+    }
+
     if (add) {
         const result = num1 + num2;
-
         return result;
     }
     else {
@@ -23,15 +26,25 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (!Array.isArray(prices) || typeof discount !== 'number') {
+        return false;
+    }
+
     if (prices.length === 0) {
         return false;
     }
     const discounted = []
     const length = prices.length;
     let discountedPrice = 0
+
     for(let i = 0; i < length; i++) {
-        discountedPrice = Math.round((prices[i] * (1 - discount)) * 100) / 100;
-        discounted.push(discountedPrice);
+        const price = prices[i];
+        if (typeof price !== 'number') {
+            return false;
+        } else {
+            discountedPrice = Math.round((prices[i] * (1 - discount)) * 100) / 100;
+            discounted.push(discountedPrice);
+        }
     }
 
     return discounted;
